aa1bbdba3922cc4f6dfa3c7a1d8e8c2f6137a123
Rick <rikuiwanaga@users.noreply.github.com>
2017-09-27 16:38:05 -0500

    changed the style to make it look cleaner

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -8,19 +8,28 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+public class ReclamationProject {
+    static String doit(String alpha,String beta) {
+        if (alpha.length() > beta.length()) {
+            String gamma = alpha; // TODO: set c to a
+            alpha = beta;
+            beta = gamma;
+        }
+        String r = (alpha.equals(beta)) ? "" : "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < alpha.length(); i++) {
+            for (int j = alpha.length() - i; j > 0; j--) {
+                for (int k = 0; k < beta.length()- j; k++) {
+                    r = (alpha.regionMatches(i, beta, k, j) && j >r.length()) ? alpha.substring(i,i + j) : r; // Do it!
+                    }
+
+                }
+
+        }
+
+        return r;
+    }
+
 }
